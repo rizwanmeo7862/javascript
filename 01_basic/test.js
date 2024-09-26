a272e3be67e03ceda0ed47bbd4327d9f9380b7a5
Muhammad Rizwan <74702050+rizwanmeo7862@users.noreply.github.com>
2024-09-26 17:37:01 +0500

    test variabls

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,2 +1,10 @@
-console.log("Rizwan")
-console.log("03017721077")
+
+
+
+let accountId = 12345
+const accountEmail = "rizwan@gmail.com"
+var accountPassword= "12345"
+accountCity = "Vehari"
+
+
+console.log(accountId)
