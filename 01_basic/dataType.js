e2b08407ea432eceabc7add5fc004840cfd381f5
Muhammad Rizwan <74702050+rizwanmeo7862@users.noreply.github.com>
2024-09-27 08:21:51 +0000

    dataTypes

---
+++ 01_basic/dataType.js
@@ -0,0 +1,12 @@
+
+"use strict"  // treat all code as newer version
+
+//alert(2+2)   // we are using node js not browse
+
+// number =  2 to power 53
+//bigint  
+//String
+//boolean = true / false
+//null = standalone value
+
+console.log(typeof "rizwan")
