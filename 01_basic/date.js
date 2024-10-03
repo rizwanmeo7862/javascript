2d7f31d14404f81ec1282fb106030454f1f04ec4
Muhammad Rizwan <74702050+rizwanmeo7862@users.noreply.github.com>
2024-10-03 09:06:53 +0000

    worked on date

---
+++ 01_basic/date.js
@@ -0,0 +1,16 @@
+
+
+
+let myDate = new Date()
+//console.log(myDate.toString())
+// Thu Oct 03 2024 08:57:14 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toTimeString())
+//console.log(myDate.getTimezoneOffset())
+
+console.log(typeof myDate);
+
+
+
