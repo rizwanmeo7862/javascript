a3e5b1124c0e5f7b2c29f3c7c2433d646bb48891
Muhammad Rizwan <74702050+rizwanmeo7862@users.noreply.github.com>
2024-10-14 09:00:58 +0000

    added file for object

---
+++ 01_basic/objects.js
@@ -0,0 +1,21 @@
+
+// object literal 
+const mySymb = Symbol("key1")
+const JsUser = {
+    name : "Rizwan",
+    email: "contactrizwanmeo@gmail.com",
+    "full name" : "Muhammad Rizwan",
+    location: "Punjab Pakistan",
+    isLogIn: false,
+    [mySymbol] : mySymb ,
+    isLoginDays: ["Monday", "Saturday"]
+}
+console.log(JsUser["full name"])
+console.log(JsUser[mySymbol])
+
+
+
+
+
+
+
